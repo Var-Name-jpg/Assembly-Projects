bbfa239016dc80bf916753ada999d2815a6e3659
Var-Name-jpg <ethanwalton0418@outlook.com>
2025-11-01 23:10:32 -0400

    Wrote it on my own

--- x86-64/Hello-World/hello.s
+++ x86-64/Hello-World/hello.s
@@ -1,26 +1,38 @@
+# File: hello.asm
+# Author: Var-Name-jpg
+# Date Created: 2025-11-1
+#
+# Purpose:
+# 	This file outputs hello world and utilizes constants and procedures
+
 .global _start
 
-.hello.str:
-	.asciz "Hello!!!\n"
-
-.equ sys_write,1
-.equ stdout,1
-.equ sys_exit,60
+# define the message to print
+.hello.text:
+	.asciz "Hello!\n"
 
 .text
 
-_writeHello:
-	movq $sys_write, %rax
-	movq $stdout, %rdi
-	leaq .hello.str, %rsi
-	movq $10, %rdx
+# setting my constants for ease of use
+.equ WRITE,	1
+.equ STDOUT,	1
+.equ EXIT,	60
+
+# procedure that prints the message to the terminal
+printMessage:
+	movq $WRITE, %rax
+	movq $STDOUT, %rdi
+	leaq .hello.text, %rsi
+	movq $8, %rdx
 	syscall
 
-_exitProgram:
-	movq $sys_exit, %rax
+# procedure that exits the program
+exitProgram:
+	movq $EXIT, %rax
 	movq $0, %rdi
 	syscall
 
+# call procedures in _start
 _start:
-	call _writeHello
-	call _exitProgram
+	call printMessage
+	call exitProgram
